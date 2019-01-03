ba118c0bd72a51fa50d1d8f05ab754717e11dc6f
Yuriy <yuriy@Yuriy-71.local>
2019-01-03 16:16:06 +0100

    FunctorK

--- src/main/scala/example/fp3d/FP3D.scala
+++ src/main/scala/example/fp3d/FP3D.scala
@@ -1,14 +1,34 @@
 package example.fp3d
 
-import scalaz.{EitherT, Monad, MonadError, \/}
+import scalaz.{EitherT, Monad, MonadError, \/, ~>}
 import scalaz.concurrent.Task
 
 class FP3D {
+
+  trait FunctorK[H[_[_]]] {
+    def mapK[F[_], G[_]](hf : H[F])(f : F ~> G) : H[G]
+  }
+
+  object FunctorK {
+    def apply[H[_[_]]](implicit H : FunctorK[H]) : FunctorK[H] = H
+  }
 
   trait FooAlgebra[F[_]] {
     def foo(a : Int) : F[String]
 
     def bar(a : String) : F[Int]
+
+    def mapK[G[_]](f : F ~> G) : FooAlgebra[G] = FunctorK[FooAlgebra].mapK(this)(f)
+  }
+
+  object FooAlgebra {
+    implicit val functorK : FunctorK[FooAlgebra] = new FunctorK[FooAlgebra] {
+      def mapK[F[_], G[_]](hf : FooAlgebra[F])(f : F ~> G) : FooAlgebra[G] = new FooAlgebra[G] {
+        def foo(a : Int) : G[String] = f(hf.foo(a))
+
+        def bar(a : String) : G[Int] = f(hf.bar(a))
+      }
+    }
   }
 
   trait BarAlgebra[F[_]] {
@@ -17,18 +37,18 @@ class FP3D {
     def bar(a : String, b : String) : F[String]
   }
 
-  trait Program[F[_]] {
-    def run(a : Int) : F[Int]
+  object BarAlgebra {
+    implicit val functorK : FunctorK[BarAlgebra] = new FunctorK[BarAlgebra] {
+      def mapK[F[_], G[_]](hf : BarAlgebra[F])(f : F ~> G) : BarAlgebra[G] = new BarAlgebra[G] {
+        def foo(a : Int, b : Int) : G[Int] = f(hf.foo(a, b))
+
+        def bar(a : String, b : String) : G[String] = f(hf.bar(a, b))
+      }
+    }
   }
 
-  class ProgramAsync(fooAlgebra : FooAlgebra[Task], barAlgebra : BarAlgebra[Task]) extends Program[Task] {
-    def run(a : Int) : Task[Int] =
-      for {
-        b <- barAlgebra.foo(a, a)
-        c <- fooAlgebra.foo(b)
-        d <- barAlgebra.bar(c, c)
-        e <- fooAlgebra(d)
-      } yield e
+  trait Program[F[_]] {
+    def run(a : Int) : F[Int]
   }
 
   class ProgramHK[F[_]](fooAlgebra : FooAlgebra[F], barAlgebra : BarAlgebra[F])(implicit F : Monad[F]) extends Program[F] {
@@ -65,8 +85,10 @@ class FP3D {
         Monad[ErrorContext].point(b)
   }
 
-  val fooService = new FooService
-  val barService = new SafeBarService
+  val barService : BarAlgebra[ErrorContext] = new SafeBarService
+  val fooService : FooAlgebra[ErrorContext] = (new FooService).mapK(new (Task ~> ErrorContext) {
+    def apply[A](fa : Task[A]) : ErrorContext[A] = EitherT(fa.attempt)
+  })
 
-  val program = new ProgramHK[???](fooService, barService)
+  val program = new ProgramHK[ErrorContext](fooService, barService)
 }
