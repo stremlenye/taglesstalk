101d0480d66f1afac547e6e8135471c36d4066ae
Yuriy <stremlenye@gmail.com>
2019-03-18 16:42:29 +0100

    MTL day

--- src/main/scala/example/fp3d/MTL.scala
+++ src/main/scala/example/fp3d/MTL.scala
@@ -2,32 +2,85 @@ package example.fp3d
 
 import java.util.UUID
 
-import cats.data.EitherT
-import cats.mtl.ApplicativeHandle
+import cats.{Monad, MonadError}
+import cats.Functor
+import cats.arrow.FunctionK
+import cats.data.{EitherT, ReaderT}
+import cats.implicits._
+import cats.instances._
+
+import scala.concurrent.Future
 import scalaz.concurrent.Task
 
 case class User(id: UUID, name : String)
 
-trait UserAlgebra[F[_]] {
+trait UserAlgebra[F[_]] { self =>
   def getUser(id : UUID) : F[User]
+
+  def mapK[G[_]](f: FunctionK[F, G]): UserAlgebra[G] =
+    new UserAlgebra[G] {
+      def getUser(id : UUID) : G[User] = f(self.getUser(id))
+    }
 }
 
-trait UserStoreAlgebra[F[_]] {
+trait UserStoreAlgebra[F[_]] { self =>
   def fetchUser(id : UUID): F[User]
+
+  def mapK[G[_]](f: FunctionK[F, G]): UserStoreAlgebra[G] =
+    new UserStoreAlgebra[G] {
+      def fetchUser(id : UUID) : G[User] = f(self.fetchUser(id))
+    }
 }
 
-trait PermissionAlgebra[F[_]] {
+trait PermissionAlgebra[F[_]] { self =>
   def isAllowed(id : UUID): F[Boolean]
+
+  def mapK[G[_]](f: FunctionK[F, G]): PermissionAlgebra[G] =
+    new PermissionAlgebra[G] {
+      def isAllowed(id : UUID) : G[Boolean] = f(self.isAllowed(id))
+    }
 }
 
-class DatabaseException extends RuntimeException
+class UserStoreConfig
 
-final class UserStore extends UserStoreAlgebra[EitherT[Task, DatabaseException, ?]] {
-  def fetchUser(id : UUID): EitherT[Task, DatabaseException,User] = ???
+final class UserStore extends UserStoreAlgebra[ReaderT[Task, UserStoreConfig, ?]] {
+  def fetchUser(id : UUID): ReaderT[Task, UserStoreConfig, User] = ???
 }
 
-class PermissionViolationException extends RuntimeException
+class SecurityServiceException extends RuntimeException
 
+final class SecurityService extends PermissionAlgebra[EitherT[Future, SecurityServiceException, ?]] {
+  def isAllowed(id : UUID): EitherT[Future, SecurityServiceException, Boolean] = ???
+}
 
+abstract class UserServiceException extends RuntimeException
+class UserServicePermissionException extends UserServiceException
+class DependencyException(t: Throwable) extends UserServiceException
 
-class UserService[F[_]]()(implicit F : ApplicativeHandle[F, Throwable])
+class UserService[F[_]](pa : PermissionAlgebra[F], us : UserStoreAlgebra[F])(
+                       implicit F : MonadError[F, UserServiceException]
+) extends UserAlgebra[F] {
+  def getUser(id : UUID) : F[User] =
+    for {
+      allowed <- pa.isAllowed(id)
+      _ <- if(allowed == false) F.raiseError[Unit](new UserServicePermissionException) else F.pure(())
+      user <- us.fetchUser(id)
+    } yield user
+}
+
+object UserService {
+  type Context[A] =          EitherT[Future, UserServiceException, A]
+  type UserStoreContext[A] = ReaderT[Task,   UserStoreConfig,      A]
+  type PermissionsContext[A] = EitherT[Future, SecurityServiceException, A]
+
+  implicit val taskFunctor : Functor[Task] = ???
+  implicit val futureMonad : Monad[Future] = ???
+
+  val userContextToContext: FunctionK[UserStoreContext, Context] = ???
+  val permissionsContextToContext: FunctionK[PermissionsContext, Context] = ???
+
+  val us : UserStoreAlgebra[Context] = new UserStore().mapK(userContextToContext)
+  val pa : PermissionAlgebra[Context] = new SecurityService().mapK(permissionsContextToContext)
+
+  val service = new UserService[Context](pa, us)
+}
